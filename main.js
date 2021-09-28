b861978a0d65d0fd022f6de81e96c2acb9cf4123
Ritwikrajmishra <76164118+Ritwikrajmishra@users.noreply.github.com>
2021-09-28 17:13:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-console.log("started");
+status1 = "";
+objectDetector = "";
+object_to_find = "";
+
+function preload()
+{
+
+}
+
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.hide();
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+
+function draw()
+{
+    image(video, 0, 0, 480, 380);
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    object_to_find = document.getElementById("object").value;
+    console.log(object_to_find);
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status1 = true;
+}
