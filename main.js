44fd3d6451bfe612d3c246f9a507baa509e5736f
Ritwikrajmishra <76164118+Ritwikrajmishra@users.noreply.github.com>
2021-09-29 18:45:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 status1 = "";
 objectDetector = "";
-object_to_find = "";
+object_name = "";
+objects = [];
 
 function preload()
 {
@@ -18,18 +19,66 @@ function setup()
 function draw()
 {
     image(video, 0, 0, 480, 380);
+    if(status1 != "")
+        {
+            objectDetector.detect(video, gotResults);
+            for (i = 0; i < objects.length; i++)
+            {
+                if(objects[i].label == object_name)
+                {
+                    objectDetector.detect(gotResults);
+                    document.getElementById("status").innerHTML = "Status : Objects detected";
+                    document.getElementById("found_or_not_found").innerHTML = object_name + " found";
+                
+
+                    fill('#FF0000');
+                    percent = floor(objects[i].confidence * 100);
+                    text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+                    noFill();
+                    stroke('#FF0000');
+                    rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+                    synth = window.speechSynthesis;
+                    speak_data = object_name + "found";
+
+                    utterThis = new SpeechSynthesisUtterance(speak_data);
+                    synth.speak(utterThis);
+                }
+
+                else
+                {
+                    document.getElementById("status").innerHTML = "Status : Objects detected";
+                    document.getElementById("found_or_not_found").innerHTML = object_name + " not found";
+                    
+                    synth = window.speechSynthesis;
+                    speak_data = object_name + "not found";
+
+                    utterThis = new SpeechSynthesisUtterance(speak_data);
+                    synth.speak(utterThis);
+                }
+            }
+        }
 }
 
 function start()
 {
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
-    object_to_find = document.getElementById("object").value;
-    console.log(object_to_find);
+    object_name = document.getElementById("object").value;
 }
 
 function modelLoaded()
 {
     console.log("Model Loaded!");
     status1 = true;
+}
+
+function gotResults(error, results)
+{
+    if(error) 
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
